fff555ebfaa5c2cfaf6e7238d1035c25c6ab40c6
Júlio Cesar Nascimento de Lisboa <julio.lisboa@mdh.gov.br>
2025-06-25 13:39:01 -0300

    atualização de next/font

--- src/app/contrato.tsx
+++ src/app/contrato.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Document, Page, Text, View, StyleSheet, Image, Font } from "@react-pdf/renderer";
+import { Document, Page, Text, View, StyleSheet, Font } from "@react-pdf/renderer";
 
 Font.register({
   family: 'Ancizar-Serif',
