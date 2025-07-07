0e145186f6dc8d9e3417cb4047a0f37f8a05e6aa
Julio <132522218+juliodf24@users.noreply.github.com>
2025-07-07 11:07:38 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,6 +91,26 @@ export default function HomeDownload() {
 
   return (
     <>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d,t) {
+                var BASE_URL="https://ec2-18-206-187-99.compute-1.amazonaws.com";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.defer = true;
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                  window.chatwootSDK.run({
+                    websiteToken: 'LESsJtCyLE4DSJmb9GfyXRkJ',
+                    baseUrl: BASE_URL
+                  })
+                }
+              })(document,"script");
+            `,
+          }}
+        />
       {!submitted ? (
         <div className=" w-screen flex flex-col items-center justify-center">
           <Image
